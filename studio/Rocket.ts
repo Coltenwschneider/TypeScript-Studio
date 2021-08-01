91d4710dc91bf838a05a6c1beee1d1f6dbab9ebd
Colten Schneider <coltenschneider@Coltens-MacBook-Pro.local>
2021-08-01 14:03:14 -0500

    added astronaut classes

---
+++ studio/Rocket.ts
@@ -0,0 +1,64 @@
+import { Astronaut } from "./Astronaut";
+import { Cargo } from "./Cargo";
+import { Payload } from "./Payload";
+
+export class Rocket {
+    name: string;
+    totalCapacityKg: number;
+    cargoItems: Cargo[] = [];
+    astronauts: Astronaut[] = [];
+
+    constructor(name: string, totalCapacityKg: number) {
+        this.name = name;
+        this.totalCapacityKg = totalCapacityKg;
+    }
+
+    sumMass(items: Payload[]): number {
+        // Returns the sum of all items using each item's massKg property
+        let massKgTotal: number = 0;
+        items.forEach(element => {
+            massKgTotal += element.massKg;
+        });
+        return massKgTotal;
+    }
+
+    currentMassKg(): number {
+        // Uses this.sumMass to return the combined mass of this.astronauts and this.cargoItems
+        return this.sumMass(this.astronauts) + this.sumMass(this.cargoItems);
+    }
+
+    canAdd(item: Payload): boolean {
+        // Returns true if this.currentMassKg() + item.massKg <= this.totalCapacityKg
+        if (this.currentMassKg() + item.massKg <= this.totalCapacityKg) {
+            return true;
+        }        
+    }
+
+    addCargo(cargo: Cargo): boolean {
+        // Uses this.canAdd() to see if another item can be added.
+        // If true, adds cargo to this.cargoItems and returns true.
+        // If false, returns false.
+        if (this.canAdd(cargo)) {
+            this.cargoItems.push(cargo);
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    addAstronaut(astronaut: Astronaut): boolean {
+         // Uses this.canAdd() to see if another astronaut can be added.
+        // If true, adds astronaut to this.astronauts and returns true.
+        // If false, returns false.
+        if (this.canAdd(astronaut)) {
+            this.astronauts.push(astronaut);
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+      
+}
+
